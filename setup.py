c5ff65169d8bdde70791a0d8119dbe546d898e41
JINHAE JUNG <jinland@bommaru.com>
2021-12-16 09:56:42 +0900

    python version 3.8.10 이상으로 수정

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='==3.8.10',
+    python_requires='>=3.8.10',
     install_requires=[
         'PyMySQL >= 1.0.2',
         'sshtunnel >= 0.4.0',
